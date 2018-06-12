e3fed743095970def1f66dcc63735b7214253502
Jiří Prokop <jprokop@synaptiko.cz>
2018-06-12 22:23:03 +0200

    Fix formatting

--- popup.js
+++ popup.js
@@ -5,7 +5,7 @@ textarea.focus();
 
 function sendText() {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-			chrome.tabs.sendMessage(tabs[0].id, { data: textarea.value });
+		chrome.tabs.sendMessage(tabs[0].id, { data: textarea.value });
 	});
 }
 
